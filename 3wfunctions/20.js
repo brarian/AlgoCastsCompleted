612b9ff1807ac688a7aa4ebc119c44520eab1c09
Brarian <b.namonywa@gmail.com>
2019-06-07 19:01:46 -0500

    unique id from list

--- 3wfunctions/20.js
+++ 3wfunctions/20.js
@@ -1,2 +1,15 @@
 //Write a JavaScript function that generates a string id
 // (specified length) of random characters.
+
+const id = x => {
+  let text = "";
+  let char_list =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890";
+
+  for (let i = 0; i < x; i++) {
+    text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  return text;
+};
+
+console.log(id(3));
