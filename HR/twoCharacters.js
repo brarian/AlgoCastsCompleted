5ca20d37b9a314a7cea5cd7a69971674693159c2
Brarian <b.namonywa@gmail.com>
2019-06-11 19:09:56 -0500

    two chars start

---
+++ HR/twoCharacters.js
@@ -0,0 +1,23 @@
+let str = "abaacdabd";
+
+const alternate = x => {
+  //unique values from string
+  let st = Array.from(new Set(x));
+
+  // all possible combinations
+  let arr = [];
+  for (let i = 0; i < x.length; i++) {
+    for (let j = i + 1; j < x.length; j++) {
+      arr.push(x.slice(i, j));
+    }
+  }
+  // we only want combos that are 2 characters in length AND are different characters
+  let combos = arr.filter(i => i.length == 2 && i[0] !== i[1]);
+  combos = combos.map(i => i.split(""));
+  //return combos
+
+  // we are going to filter combos from our string
+  x = x.split("");
+};
+
+alternate(str);
