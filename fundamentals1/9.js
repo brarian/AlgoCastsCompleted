dd108f0f3fac4b363967eb0eb1d15d87af035803
Brarian <b.namonywa@gmail.com>
2019-07-19 10:23:52 -0500

    powerset

---
+++ fundamentals1/9.js
@@ -0,0 +1,19 @@
+//Write a JavaScript program to combine the numbers of
+//a given array into an array containing all combinations.
+
+// for (let i = 0; i < x.length; i++) {
+//   for (let j = i + 1; j < x.length; j++) {
+//     arr.push(x.slice(i, j));
+//   }
+// }
+const fn = arr => {
+  let newArr = [];
+  for (let i = 0; i < arr.length + 1; i++) {
+    for (let j = i + 1; j < arr.length + 1; j++) {
+      newArr.push(arr.slice(i, j));
+    }
+  }
+  return newArr;
+};
+
+console.log(fn([1, 2, 3]));
