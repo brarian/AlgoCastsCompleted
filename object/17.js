7d63c855ffc59544c8e27eebe72900ea1ab1d870
Brarian <b.namonywa@gmail.com>
2019-09-14 11:27:21 -0500

    object 17

---
+++ object/17.js
@@ -0,0 +1,11 @@
+//Write a JavaScript function to check if an object contains given property.
+
+const fx = (obj, property) => {
+  return Object.keys(obj).includes(property) ? true : false;
+};
+
+console.log(
+  fx({ red: '#FF0000', green: '#00FF00', white: '#FFFFFF' }, 'green')
+);
+
+console.log(fx({}, 'green'));
