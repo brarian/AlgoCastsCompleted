1f08728c951c9567e62a5a3e13f65662d7f9c874
Brarian <b.namonywa@gmail.com>
2019-10-06 22:15:00 -0500

    duplicate

---
+++ iC/which-appears-twice.js
@@ -0,0 +1,12 @@
+function findRepeat(numbers) {
+  // Find the number that appears twice
+  let unique = new Set();
+
+  for (let i of numbers) {
+    if (!unique.has(i)) {
+      unique.add(i);
+    } else return i;
+  }
+
+  return 0;
+}
