50f608208f9e5ff340f042faaec345ee642ec87c
Brarian <b.namonywa@gmail.com>
2019-10-09 22:21:11 -0500

    second try of unique substring problem

--- EasyLC/Longest SubstringWithoutRepeatingCharacters.js
+++ EasyLC/Longest SubstringWithoutRepeatingCharacters.js
@@ -15,3 +15,22 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 lengthOfLongestSubstring('dvdf');
+
+//second try
+var lengthOfLongestSubstring = function(s) {
+  let start = 0;
+  let unique = '';
+  let max = 0;
+  let counter;
+  for (let char = start; char < s.length; char++) {
+    if (!unique.includes(s[char])) {
+      unique += s[char];
+      console.log(unique);
+      max = Math.max(max, unique.length);
+    } else {
+      unique = '';
+      start += 1;
+    }
+  }
+  return max;
+};
