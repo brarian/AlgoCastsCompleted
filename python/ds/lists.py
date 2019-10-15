96743dab0bca93f5866cf3a4bc031fb24a9b73a2
Brarian <b.namonywa@gmail.com>
2019-10-14 22:51:21 -0500

    lists start

---
+++ python/ds/lists.py
@@ -0,0 +1,17 @@
+letters = ["a", "b", "c"]
+matrix = [[3, 4, 5], [8, 9, 0]]
+zeros = [0] * 100
+# creates array of 100 0's
+combined = matrix + letters
+# [3, 4, 5], [8, 9, 0], 'a', 'b', 'c']
+# print(combined)
+
+numbers = list(range(20))
+# print(numbers)
+# prints all numbers 0 - 20, not including 20
+
+#l = list("hello world")
+# returns an array of hello world
+
+chars = len(list("hello world"))
+# print(chars) - returns char count
