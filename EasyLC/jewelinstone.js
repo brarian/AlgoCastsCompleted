59cb3d65035e7c67c74c5c5a2661eac41458b3d0
Brarian <b.namonywa@gmail.com>
2019-10-23 20:21:03 -0500

    jewel in stones variation

--- EasyLC/jewelinstone.js
+++ EasyLC/jewelinstone.js
@@ -15,3 +15,16 @@ var numJewelsInStones = function(J, S) {
 
   return count || 0;
 };
+
+var numJewelsInStones = function(J, S) {
+  if (J.length === 0 || S.length === 0) {
+    throw new Error('no stones or jewels');
+  }
+  let sum = 0;
+  let sArray = S.split('');
+
+  for (let i of J) {
+    sum += sArray.filter(el => el === i).length;
+  }
+  return sum;
+};
