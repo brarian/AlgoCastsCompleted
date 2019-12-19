111fc37b18fae0ebcf4dd895c503eab6791478a6
Brarian <b.namonywa@gmail.com>
2019-12-18 23:28:20 -0600

    7 completed

--- 3wSQL/BooleanAndRelationalOperators.sql
+++ 3wSQL/BooleanAndRelationalOperators.sql
@@ -10,3 +10,6 @@ WHERE NOT (ord_date = '2012-09-10 ' AND salesman_id=505)
 
 -- 7.Write a SQL statement to display salesman_id, name, city and commission 
 -- who gets the commission within the range more than 0.10% and less than 0.12%.  
+SELECT *
+FROM salesman
+WHERE commission BETWEEN '.10' AND '.12';
